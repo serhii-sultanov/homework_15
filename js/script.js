611685b4d92a3e01bf56df65f220584dc081f3e9
serhii-sultanov <sshsultanov@gmail.com>
2022-09-08 11:14:02 +0300

    replaced const albumsList

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 "use strict";
 
+const albumsList = document.getElementById("albums");
+
 fetch("https://jsonplaceholder.typicode.com/albums")
   .then((response) => response.json())
   .then((albums) => {
     albums.forEach((album) => {
       const { title } = album;
-      const albumsList = document.getElementById("albums");
       let albumsListItem = document.createElement("li");
       albumsListItem.classList.add("album_title");
       albumsListItem.innerHTML = title;
