025190d71b1995cd5b2abed889e92000f7ab4380
serhii-sultanov <sshsultanov@gmail.com>
2022-09-09 18:13:46 +0300

    added new buttons and an event

--- js/script.js
+++ js/script.js
@@ -2,15 +2,28 @@
 
 const albumsList = document.getElementById("albums");
 
+albumsList.onclick = (event) => {
+  const isRemoveButton = event.target.className === "remove_button";
+  if (isRemoveButton) {
+    event.target.closest(".album_title").remove();
+  }
+};
+
 fetch("https://jsonplaceholder.typicode.com/albums")
   .then((response) => response.json())
   .then((albums) => {
     albums.forEach((album) => {
       const { title } = album;
-      let albumsListItem = document.createElement("li");
+      const albumsListItem = document.createElement("li");
       albumsListItem.classList.add("album_title");
       albumsListItem.innerHTML = title;
       albumsList.append(albumsListItem);
+
+      const removeButton = document.createElement("button");
+      removeButton.classList.add("remove_button");
+      removeButton.setAttribute("type", "submit");
+      removeButton.innerHTML = "Delete";
+      albumsListItem.append(removeButton);
     });
   })
   .catch((error) => console.error(error));
